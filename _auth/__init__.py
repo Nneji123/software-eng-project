4fb7e293ccf19156fd145a3b1c4bab8b3085f1b8
Nneji123 <ifeanyinneji777@gmail.com>
2022-11-10 16:28:50 +0100

    updated files

--- _auth/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Authentication Based API Security with FastAPI and Postgres Database or Sqlite3 Database.
-"""
-
-from _auth.endpoints import api_key_router
-from _auth.security_api_key import api_key_security
--- app.py
+++ app.py
@@ -1,85 +1,96 @@
-from _auth import api_key_router, api_key_security
-from fastapi import FastAPI, File, Form, Request, Response, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from starlette.responses import FileResponse
+import re
+from flask import Flask, render_template, request, redirect, url_for, session
+from database import insert_user, retrieve_users
+import sqlite3 as sql
+import cv2
+import io
+from PIL import Image
+from utils import *
+
+app = Flask(__name__)
+app.secret_key = 'testsecret'
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
+        account = cur.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            session['email'] = account['email']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+ 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+ 
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        # account = retrieve_users()
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute('SELECT * FROM users WHERE username=? OR email=?', (username, email))
+        account = cur.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cur.execute('INSERT INTO  users VALUES (NULL, ?, ?, ?)', (username, password, email, ))
+            msg = 'You have successfully registered !'
+            return render_template('index.html')  
+        
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
 
 
-# from utils import *
 
-app = FastAPI()
-app.include_router(api_key_router)
-
-app = FastAPI(
-    title="Cartoonify API Backend",
-    description="""A simple API to convert images to cartoonified images.""",
-    version="0.0.1",
-    redoc_url=None,
-)
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-templates = Jinja2Templates(directory='templates/')
-app.mount('/templates/static', StaticFiles(directory="static"), name="static")
-
-# favicon_path = "templates/assets/favicon.ico"
+@app.route('index', methods=['GET', 'POST'])
+async def upload():
+    if request.method == "POST":
+        form = await request.form()
+        submit= ''
+        if form["file"]:
+            files = form["file"]
+            contents = io.BytesIO(await files.read())
+            file_bytes = np.asarray(bytearray(contents.read()), dtype=np.uint8)
+            img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+            cv2.imwrite("./images/image.jpg", img)
+            try:
+                image = Image.open('./images/image.jpg')
+                image = np.array(image)
+                inference(image=image)
+                return submit
+            except ValueError:
+                vals = "Error! Please upload a valid image type."
+                return vals
 
 
-# @app.get("/favicon.ico", include_in_schema=False)
-# async def favicon():
-#     return FileResponse(favicon_path)
 
-
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get("/index.js")
-def index_js(request: Request):
-    return templates.TemplateResponse("static/index.js", {"request": request})
-
-@app.get("/upload")
-def upload(request: Request):
-    return templates.TemplateResponse("upload.html", {"request": request})
-
-@app.get("/signup")
-def signup(request: Request):
-    return templates.TemplateResponse("signup.html", {"request": request})
-
-
-# @app.post("/signin")
-# async def signin(request: Request):
-#     if request.method == "POST":
-#         form = await request.form()
-#         if form["email"]:
-#             text = form["email"]
-#             email_validate(text)
-#         # else:
-            
-# @app.post("/translate")
-# async def home(request: Request):
-#     sumary = ""
-#     if request.method == "POST":
-#         form = await request.form()
-#         if form["message"] and form["language"]:
-#             language = form["language"]
-#             text = form["message"]
-#             translate = get_translation(language, text)
-#             sumary = " ".join(translate)
-
-#     return templates.TemplateResponse(
-#         "translation.html",
-#         {"request": request, "message": text, "language": language, "sumary": sumary},
-#     )
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
